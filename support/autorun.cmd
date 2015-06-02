ccfb461d18964b9d6c0f36cf015f75c1448869d2
Sune Gynthersen <sune.gynthersen@gmail.com>
2015-06-02 15:54:25 +0200

    Improved autorun.cmd template. Includes sublime text fix, and path setup

--- support/autorun.cmd
+++ support/autorun.cmd
@@ -1,5 +1,9 @@
 @echo off
 doskey ll=ls -l
-doskey cdw=cd C:\repositories
+doskey cdr=cd %USERPROFILE%\repositories
 doskey cdh=cd %USERPROFILE%
 doskey clear=cls
+doskey subl="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
+
+SET PATH=%PATH%;C:\Program Files (x86)\NUnit 2.6.3\bin;C:\Python27;C:\Python27\Scripts;C:\Program Files\nodejs
+SET PYTHONPATH=%PYTHONPATH%;.
